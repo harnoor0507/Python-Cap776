52ddbd0da1abe3ba85bf0ad73985ce7d82e4fedb
harnoor0507 <harnoors1111@gmail.com>
2024-08-27 23:07:18 +0530

    Add files via upload

---
+++ 7aug(2).py
@@ -0,0 +1,13 @@
+#Sandbox#1
+
+
+totalprizes=53
+numberOfStudents=8
+prizesPerStudent=totalPrizes//numberOfStudents
+prizesLeftOver=totalprizes%numberOfStudents
+print(f"Each student gets:"{prizesPerStudent})
+print(f"Prizes Left:"{prizesLeftOver})
+
+
+
+
